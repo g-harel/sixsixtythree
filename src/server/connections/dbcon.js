194691584b804d162706a83aa5a06d1b369c5b3f
Gabriel <gabrielj.harel@gmail.com>
2018-12-15 23:54:11 -0500

    add docker-compose to manage local mongo

--- src/server/connections/dbcon.js
+++ src/server/connections/dbcon.js
@@ -2,14 +2,19 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 const collectionName = 'rooms';
-const mongoUrl = 'mongodb://localhost:27017/663';
+const mongoUrl = 'mongodb://localhost:27017';
+const auth = {
+    user: 'root' || process.env.MONGO_USER,
+    password: 'root' || process.env.MONGO_PASS,
+};
 
 const defaultState = {showCompleted: true, tasks: []};
 
 const dbcon = (callback) => {
-    MongoClient.connect(mongoUrl, function(err, db) {
+    MongoClient.connect(mongoUrl, {auth}, function(err, db) {
         if (err) {
-            handleErr(err);
+            console.error(err);
+            process.exit(1);
         }
 
         const readRoom = (roomId, handleErr, callback) => {
