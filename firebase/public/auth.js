c26372a70a2b78b4eae5f5c64985ce70cb1bab4e
g-harel <gabrielj.harel@gmail.com>
2020-02-11 22:10:26 -0600

    add minimal login and logout integration

---
+++ firebase/public/auth.js
@@ -0,0 +1,47 @@
+function findOrDie(selector) {
+    const element = document.querySelector(selector);
+    if (element === null) {
+        throw new Error("missing required element: " + selector);
+    }
+    return element;
+}
+
+function handleErr(message) {
+    return function (err) {
+        console.error(message, JSON.stringify(err, null, 2));
+    }
+}
+
+var loginButton = findOrDie("#login");
+var logoutButton = findOrDie("#logout");
+var userContainer = findOrDie("#user");
+
+function render(user) {
+    if (user) {
+        loginButton.style.display = "none";
+        logoutButton.style.display = "block";
+        userContainer.style.display = "block";
+        userContainer.innerHTML = user.displayName;
+    } else {
+        loginButton.style.display = "block";
+        logoutButton.style.display = "none";
+        userContainer.style.display = "none";
+    }
+}
+
+loginButton.onclick = function () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().useDeviceLanguage();
+    firebase.auth().signInWithRedirect(provider);
+};
+
+logoutButton.onclick = function () {
+    firebase.auth().signOut().then(function () {
+        console.log("signed out");
+    }).catch(handleErr("sign out"));
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    console.log(user);
+    render(user);
+});
