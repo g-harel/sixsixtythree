168e33a7bf207e6e9052da61abdb3fd4dc89e81c
g-harel <gabrielj.harel@gmail.com>
2020-02-20 22:48:38 -0600

    add styles for logout button

--- firebase/scripts/components/Header.tsx
+++ firebase/scripts/components/Header.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 import {useAuth, login, logout} from "../internal/auth";
 
+const transitionDurationSeconds = 0.2;
+
 const HeaderWrapper = styled.div`
     display: flex;
     flex-direction: row;
@@ -28,7 +30,6 @@ const LogoPlaceholder = styled.div`
 
 const ProfileIcon = styled.div`
     align-items: center;
-    align-self: flex-end;
     background-color: #e6dcbd;
     border-radius: 0.2rem;
     display: flex;
@@ -48,15 +49,43 @@ const ProfileIcon = styled.div`
     }
 `;
 
-const Logout = styled.div`
+const LogoutWrapper = styled.div`
     align-items: center;
+    border-radius: 0.2rem;
     display: flex;
     flex-direction: column;
     flex-grow: 1;
-    flex-shrink: 1;
     justify-content: center;
+    margin-right: 0;
+    margin: 0.45rem;
+    min-width: 0;
     opacity: 0;
-    transition: opacity 0.2s ease;
+    overflow: hidden;
+    padding: 0 0.5rem;
+    pointer-events: none;
+    transition: opacity ${transitionDurationSeconds / 2}s ease;
+
+    &:hover {
+        background-color: #eee;
+        cursor: pointer;
+    }
+`;
+
+const LogoutHint = styled.div`
+    color: #665863;
+    font-size: 1rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    white-space: nowrap;
+`;
+
+const LogoutEmail = styled.span`
+    color: #968893;
+    font-size: 0.7rem;
+    font-weight: 700;
+    max-width: 100%;
+    text-overflow: ellipsis;
+    overflow: hidden;
 `;
 
 const Profile = styled.div`
@@ -66,21 +95,26 @@ const Profile = styled.div`
     flex-direction: row-reverse;
     height: 3.5rem;
     margin: 0.5rem;
-    overflow: hidden;
-    transition: background-color 0.2s ease, border-color 0.2s ease, width 0.2s ease, height 0.2s ease;
+    transition: background-color ${transitionDurationSeconds}s ease,
+        border-color ${transitionDurationSeconds}s ease,
+        width ${transitionDurationSeconds}s ease,
+        height ${transitionDurationSeconds}s ease;
     width: 3.5rem;
 
     &.open {
         background-color: #ffffff;
-        width: 10rem;
+        width: 12em;
 
         ${ProfileIcon} {
             border: 0.05rem solid #e6dcbd;
             margin: 0.45rem;
         }
 
-        ${Logout} {
+        ${LogoutWrapper} {
             opacity: 1;
+            pointer-events: all;
+            transition-delay: ${transitionDurationSeconds}s;
+            transition-duration: ${transitionDurationSeconds / 2}s;
         }
     }
 `;
@@ -105,6 +139,11 @@ export const Header: React.FunctionComponent = (props) => {
 
     const profileLetter = (user.email || "!")[0];
 
+    const onLogout = () => {
+        setTimeout(logout, 2 * transitionDurationSeconds * 1000);
+        setIsOpen(false);
+    };
+
     return (
         <HeaderWrapper>
             <AlwaysContent>
@@ -116,9 +155,10 @@ export const Header: React.FunctionComponent = (props) => {
                     <ProfileIcon onClick={() => setIsOpen(!isOpen)}>
                         {profileLetter}
                     </ProfileIcon>
-                    <Logout>
-                        <button onClick={logout}>logout</button>
-                    </Logout>
+                    <LogoutWrapper onClick={onLogout}>
+                        <LogoutHint>Sign Out</LogoutHint>
+                        <LogoutEmail>{user.email}</LogoutEmail>
+                    </LogoutWrapper>
                 </Profile>
             </AlwaysContent>
         </HeaderWrapper>
