9d5106c262fd956ccbc62c3f295d812872d73c70
g-harel <gabrielj.harel@gmail.com>
2020-02-27 01:03:18 -0600

    remove code from previous implementation

--- src/client/reloadcon.js
+++
@@ -1,4 +0,0 @@
-module.exports = () => {
-    const socket = window.io.connect(window.location.origin, {reconnectionDelayMax: 500, reconnectionAttempts: 25});
-    socket.on('reload', () => location.reload());
-};
